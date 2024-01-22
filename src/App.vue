84b135672dada0ae92d9d568ab160ae461af5a93
s7uvx <william.nash@students.iaac.net>
2024-01-21 22:22:59 -0800

    updated with colour controls

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,47 @@ import { ref } from "vue"
 const newText = ref('')
 let outputText = ref('')
 
-let selector = ref('')
-let colorSelection = ref('')
+let selector = ref('#title')
+let colourSelection = ref('')
 
 
 function updateText() {
 
     outputText.value += newText.value
     outputText.value += '\n'
+    newText.value = ""
 }
 
-// function updateColors() {
+function clearText() {
 
+    outputText.value = ""
 
-// }
+}
+
+function updateCSS() {
+    const elements = document.querySelectorAll(selector.value)
+    elements.forEach((element) => {
+        //change the colour of the element
+        element.style.background = colourSelection.value
+    })
+}
+
+const x =ref(0)
+const s = ref("80%")
+const l = ref("50%") 
+
+function onMousemove(e) {
+    x.value = e.clientX
+    colourSelection.value = `hsl(${x.value}, ${s.value}, 50%)`
+}
+
+function setToNavbar() {
+    selector.value = "#navbar"
+}
+
+function setToMain() {
+    selector.value = "#main"
+}
 
 </script>
 
@@ -30,7 +57,7 @@ function updateText() {
 <!-- the template is where the html code goes -->
 <template>
 
-<div id="navbar" class="container">
+<div id="navbar" class="container" @click="setToNavbar">
       <div id="title">It's a cow!</div>
       <div id="logo">
         <img src="\src\assets\cow.jpg" alt="macad cow" />
@@ -39,12 +66,23 @@ function updateText() {
 
     <div id="flex">
       <div id="sidebar" class="container">
-        <input type='text' v-model="newText" placeholder="Text" />
+        <input type='text' v-model="newText" placeholder="Add Text" v-on:keyup.enter="updateText"/>
         <button @click="updateText"> Add text </button>
-        <!-- <p>{{ outputText }}</p> -->
+        <button @click="clearText"> Clear text</button>
+        <p><br></p>
+        <input type='text' v-model="selector" placeholder="element type" />
+        <div
+            @mousemove="onMousemove"
+            @click="updateCSS"
+            class = "movearea">
+        <p>Click to set colour</p>
+        </div>
+            
+        <input type='text' v-model="colourSelection" placeholder="new background colour" />
+        <button @click="updateCSS">Change background</button>
       </div>
 
-      <div id="main" class="container">
+      <div id="main" class="container" @click="setToMain">
         <!-- <p> input text is {{ newText }} </p> -->
         <p class="newlineStringStyle"> <pre>{{ outputText }}</pre> </p>
 
@@ -82,6 +120,11 @@ img{
 
 }
 
+input{
+    width: 90%;
+}
+
+
 #navbar{
 
     height: 100px;
@@ -98,13 +141,13 @@ img{
 
 #sidebar{
     
-    width:30%;
+    width:20%;
     border-color: blue;
 
 }
 
 #main{
-    width:70%;
+    width:80%;
     border-color: green;
 
 }
@@ -112,6 +155,7 @@ img{
 #title{
     width: 70%;
     float:left;
+    font-size: 80px;
 }
 
 #logo{
@@ -126,12 +170,23 @@ img{
 
     border-style: dotted;
     border-width: 1px;
-    color: v-bind('colorSelection')
+    
 
 }
 
 .newlineStringStyel {
     white-space: pre-wrap;
+}
+
+.movearea {
+  background-color: v-bind(colourSelection);
+  transition: 0.3s background-color ease;
+  height: 50px;
+  padding: 10px;
+  border-radius: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 </style>
---
+++ src/AppDropDownContent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="active">
+      <slot/>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'AppDropdownContent',
+      inject: ['sharedState'],
+      computed: {
+        active () {
+          return this.sharedState.active
+        }
+      }
+    }
+  </script>
